9cfbb750ff8676c263437bdeaec2be2580421727
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2022-11-20 04:34:41 -0300

    perf(setup): improve usage of copy command to handle all files and folders inside shared folder

--- scripts/copy_docker_files.sh
+++ scripts/copy_docker_files.sh
@@ -4,20 +4,13 @@ function copy_docker_files {
   local directories
   directories=("$(ls -d */)")
 
-  local files=(
-    Dockerfile
-    .dockerignore
-  )
-
   for directory in $directories; do
     case $directory in
       scripts/|shared/) continue ;;
-      *) printf "Copying files to './%s'\n" "$directory" ;;
+      *) printf "Copying files and folders to './%s'\n" "$directory" ;;
     esac
 
-    for file in "${files[@]}"; do
-      cp ./shared/"$file" ./"$directory"
-    done
+    cp -r ./shared/ ./"$directory"
   done
 }
 
