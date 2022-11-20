b8e4f8aa38b15c5ad89e449012539f9204d35f9f
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2022-11-20 05:25:11 -0300

    feat(setup): add devcontainer files when copying docker related files

--- scripts/copy_docker_files.sh
+++ scripts/copy_docker_files.sh
@@ -11,7 +11,37 @@ function copy_docker_files {
     esac
 
     cp -r ./shared/ ./"$directory"
+    handle_template_files "$directory"
   done
+}
+
+function handle_template_files {
+  local directory=$1
+
+  SERVICE_NAME=$(basename "$directory")
+  export SERVICE_NAME
+
+  local templates_paths=(
+    .devcontainer/compose.devcontainer.yml
+  )
+
+  for template_path in "${templates_paths[@]}"; do
+    create_file_from_template \
+      ./"$directory"/"$template_path" \
+      ./shared/"$template_path"
+  done
+
+  unset SERVICE_NAME
+}
+
+function create_file_from_template {
+  local destionation_path=$1
+  local template_path=$2
+
+  eval "cat << EOF > $destionation_path
+$(<$template_path)
+EOF
+  " 2> /dev/null
 }
 
 copy_docker_files
