0a0651f4d61474a2ed790f00ed40809cdf66a37e
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2022-12-13 10:52:32 -0300

    wip(get-payment): add general structure

--- scripts/get_repositories.sh
+++ scripts/get_repositories.sh
@@ -8,6 +8,8 @@ function get_repositories {
     br-ms-sd00054-oi00005-ambassadorcorewallet
     br-ms-sd00054-oi00003-ambassadorremittances
     br-ms-sd00103-oi00011-reversetechniquecorewallet
+    # TODO: Add service name when BIAN is ready
+    # get-payment
   )
 
   for repository in "${repositories[@]}"; do
