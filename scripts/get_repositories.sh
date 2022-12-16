b52d3530e03db8c2dc33ecad4f5bd569dbfc3cb3
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2022-12-15 21:11:24 -0300

    feat(setup): add get information charge project

--- scripts/get_repositories.sh
+++ scripts/get_repositories.sh
@@ -4,6 +4,7 @@ function get_repositories {
   local repositories=(
     br-ms-sd00054-oi00005-ambassadorcorewallet
     br-ms-sd00103-oi00007-createpaymentcorewallet
+    br-ms-sd00103-oi00013-getinformationcharge
   )
 
   for repository in "${repositories[@]}"; do
