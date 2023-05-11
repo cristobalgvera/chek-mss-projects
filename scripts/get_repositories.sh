5d8758eb2bcb3193be797378673cc63446bfb388
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2023-05-11 12:37:04 -0400

    feat(compose): change name to process wallet authorization service

--- scripts/get_repositories.sh
+++ scripts/get_repositories.sh
@@ -15,6 +15,7 @@ function get_repositories {
     br-ms-sd00103-oi00016-savemovements
     br-ms-sd00103-oi00017-getmovements
     br-ms-sd00025-oi00004-sendemail
+    br-ms-sd00054-oi00007-processwalletauthorization
   )
 
   for repository in "${repositories[@]}"; do
